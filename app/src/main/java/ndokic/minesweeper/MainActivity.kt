25f1c94688689bcf824d3b8792e967535784baf1
nnnbg87 <nnnbg87@hotmail.com>
2019-03-19 17:54:06 +0100

    Created Game Activity

--- app/src/main/java/ndokic/minesweeper/MainActivity.kt
+++ app/src/main/java/ndokic/minesweeper/MainActivity.kt
@@ -1,12 +1,17 @@
 package ndokic.minesweeper
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun onClickEasy(v : View) {
+        startActivity(Intent(this, GameActivity::class.java))
     }
 }
