ba6fc219b5e94d09936ab0eee287170814670922
nnnbg87 <nnnbg87@hotmail.com>
2019-03-19 17:47:43 +0100

    created project

---
+++ app/src/main/java/ndokic/minesweeper/MainActivity.kt
@@ -0,0 +1,12 @@
+package ndokic.minesweeper
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
